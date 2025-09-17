806971236148743be7d3bc4df3b4175fe8783038
Parker Hinman <phzd3@umsystem.edu>
2025-09-17 17:08:17 -0500

    Added a lot of imports, messed around with Dash some more

--- PaperTradingProject/PaperTradingAlgorithm.py
+++ PaperTradingProject/PaperTradingAlgorithm.py
@@ -2,13 +2,19 @@ import alpaca as tradeAPI
 import os
 from dotenv import load_dotenv
 import datetime
+import pandas_datareader as pdr
 import yfinance as yf
-from dash import html, Dash
+from dash import html, Dash, dcc
 from dash.dependencies import Input,Output
+import keyboard
 load_dotenv()
 
 API_KEY_SECRET = os.getenv('API_KEY_SECRET')
 API_KEY = os.getenv('API_KEY')
+
+while True:
+    if keyboard.is_pressed('q'):
+        exit
 
 def trading_algo():
     os.environ['APCA_API_BASE_URL'] = 'https://paper-api.alpaca.markets'
@@ -21,7 +27,31 @@ def trading_algo():
 app = Dash()
 app.title = "Stock Selector and Visualization"
 
-app.layout = [html.Div(children="Hello World")]
+app.layout = [html.Div(children=[
+    html.H1("Stock Data Visualization Dashboard"),
+    html.H4("Please enter the stock name"),
+    dcc.Input(id='stockSymbolInput', value='AAPL', type="text"),
+    html.Div(id='output-graph')
+    ])]
+@app.callback(
+    Output(component_id='output-graph',
+component_property='children'),
+[Input(component_id='input', component_property='value')]
+)
+
+def update_graph(input_data):
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime.now()
+
+    try:
+        df = pdr.DataReader(input_data, 'yahoo', start, end)
+
+        graph = dcc.Graph(id="example", figure ={'data':[{'x':df.index, 'y':df.Close, 'type':'line', 'name':{'title':input_data}}],'layout':{'title':input_data}})
+
+    except:
+        graph= html.Div('Error retrieving stock data.')
+
+    return graph
 
 if __name__ == '__main__':
     app.run(debug=True)
