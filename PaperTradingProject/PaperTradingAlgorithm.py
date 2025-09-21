3dea1699b673a3afbdd8ec19f03c82fad9662d30
Parker Hinman <phzd3@umsystem.edu>
2025-09-21 15:07:45 -0500

    initializing app layout

--- PaperTradingProject/PaperTradingAlgorithm.py
+++ PaperTradingProject/PaperTradingAlgorithm.py
@@ -1,20 +1,21 @@
+#To/Do Learn Dash, mess around with graphs
+#To/Do connect with stock data
+#To/Do create Trading Algorithm
+
 import alpaca as tradeAPI
 import os
 from dotenv import load_dotenv
 import datetime
-import pandas_datareader as pdr
-import yfinance as yf
-from dash import html, Dash, dcc
-from dash.dependencies import Input,Output
-import keyboard
+from alpha_vantage.timeseries import TimeSeries
+from dash import html, Dash, dcc, callback, State, Input, Output
+import pandas as pd
+import plotly.express as px
+
+
 load_dotenv()
 
 API_KEY_SECRET = os.getenv('API_KEY_SECRET')
 API_KEY = os.getenv('API_KEY')
-
-while True:
-    if keyboard.is_pressed('q'):
-        exit
 
 def trading_algo():
     os.environ['APCA_API_BASE_URL'] = 'https://paper-api.alpaca.markets'
@@ -24,34 +25,24 @@ def trading_algo():
 
     return
 
-app = Dash()
-app.title = "Stock Selector and Visualization"
+df = pd.read_csv()
 
-app.layout = [html.Div(children=[
-    html.H1("Stock Data Visualization Dashboard"),
-    html.H4("Please enter the stock name"),
-    dcc.Input(id='stockSymbolInput', value='AAPL', type="text"),
-    html.Div(id='output-graph')
-    ])]
-@app.callback(
-    Output(component_id='output-graph',
-component_property='children'),
-[Input(component_id='input', component_property='value')]
+app = Dash()
+
+app.title = 'Stock Trading Dashboard'
+app.layout = [  html.Div(children=(
+                    dcc.Input(id='stockTickerInput',type='text',placeholder='Ex. APPL'))), 
+                html.Div(children=(  
+                    dcc.Graph(id='stockHistoryGraph')
+                ))]
+
+@callback(
+    Input('stockTickerInput', 'value'),
+    Output('stockHistoryGraph', 'children')
 )
 
-def update_graph(input_data):
-    start = datetime.datetime(2010, 1, 1)
-    end = datetime.datetime.now()
-
-    try:
-        df = pdr.DataReader(input_data, 'yahoo', start, end)
-
-        graph = dcc.Graph(id="example", figure ={'data':[{'x':df.index, 'y':df.Close, 'type':'line', 'name':{'title':input_data}}],'layout':{'title':input_data}})
-
-    except:
-        graph= html.Div('Error retrieving stock data.')
-
-    return graph
+def update_graph():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
